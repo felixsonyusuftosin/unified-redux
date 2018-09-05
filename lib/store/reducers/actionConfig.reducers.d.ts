8e3df1241fae6181c931402004d4b32646c474ac
felixsonyusuftosin <felixsonyusuftosin@gmail.com>
2018-09-05 14:34:15 +0100

    add npm ignpore file

---
+++ lib/store/reducers/actionConfig.reducers.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @file define generic reducers
+ */
+/**
+ * Local imports
+ */
+import { IkeyValuePair } from "../../types/index";
+import { IdispatchParam } from "../actions/actionConfig";
+export interface IactionType<T> extends IdispatchParam<T> {
+    payload: T;
+}
+/**
+ * @function returns reducers that represents the variables defined in the actionDictionaryConstants
+ * @return  { any } - return values
+ */
+export declare const reducerObject: (actionDictionary: IkeyValuePair) => IkeyValuePair;
